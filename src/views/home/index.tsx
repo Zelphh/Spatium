4b68c7b0363ffa9739cd13e2c107725b98503bf1
Matheus Melle Tártari <matheusmelle2006@gmail.com>
2026-02-08 16:58:33 -0300

    Feitas configurações iniciais, iniciada construção da tela de home.

---
+++ src/views/home/index.tsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <>
+      <h1>Welcome to Spatium</h1>
+      <div className="w-100 card">
+        Testando card
+      </div>
+    </>
+  );
+}
+
+export default Home;
