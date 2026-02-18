7994d4bb33f644bfa94d8419de3bfcf4482d3701
Matheus Melle Tártari <matheusmelle2006@gmail.com>
2026-02-18 00:08:34 -0300

    fix: Ajuste nas bibliotecas do projeto.

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      prettier: prettierPlugin,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      // React
+      'react/react-in-jsx-scope': 'off',
+
+      // Hooks
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      'prettier/prettier': 'warn',
+    },
+  },
+  prettierConfig,
+];
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -7,10 +7,11 @@ function Home() {
     <>
       <div className="p-5 w-100">
         <div className="d-flex align-items-center gap-3 mb-4">
-          <Clock size={30}/>       
-          <h2 className='m-0 p-0'>Welcome to Spatium</h2>
+          <Clock size={30} />
+          asdasdad asdasds
+          <h2 className="m-0 p-0">Welcome to Spatium</h2>
         </div>
-        <Row className='flex-nowrap'>
+        <Row className="flex-nowrap">
           <Colxx xxl="4">
             <div className="card p-2"> Card 1 </div>
           </Colxx>
